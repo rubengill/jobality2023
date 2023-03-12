7cfb869e8b9e798f26072214eafac7b6d01bc143
chelseaY119 <ayang106@my.bcit.ca>
2023-03-12 01:54:43 -0800

    change height on q pages

---
+++ scripts/skeleton_q4.js
@@ -0,0 +1,6 @@
+function loadSkeleton() {   
+        console.log($('#navbarPlaceholder').load('./text/nav.html'));
+        console.log($('#footerPlaceholder').load('./text/footer.html'));
+}
+loadSkeleton(); //invoke the function
+    
